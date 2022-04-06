59df17c7a21e1c3fa7a0b9a35f55354374132691
Dominik <dmitterer@tsn.at>
2022-04-06 11:03:14 +0200

    Implementierung von "SaveLoadTasklistTests"
    Ergänzung von "TaskListTests"

--- src/test/java/SaveLoadTasklistTests.java
+++ src/test/java/SaveLoadTasklistTests.java
@@ -1,3 +1,20 @@
+import domain.TaskDescription;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SaveLoadTasklistTests {
     // Alle Methoden der File-Management-Klasse mit maximaler Code-Coverage sind zu testen
+
+    @Test
+    public void DescriptionHolen() throws IllegalArgumentException {
+
+        List<TaskDescription> descr = new ArrayList<>();
+        descr.add(new TaskDescription("Dies ist eine Beschreibung vom Task 1"));
+        Assertions.assertEquals(descr, (descr));
+
+    }
+
 }
