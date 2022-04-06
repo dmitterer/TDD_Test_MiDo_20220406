ef21786160537bef4cc5412bb55c98af6e11fa96
Dominik <dmitterer@tsn.at>
2022-04-06 10:22:15 +0200

    Implementierung von "TasksTest"

--- src/test/java/TasksTest.java
+++ src/test/java/TasksTest.java
@@ -1,4 +1,31 @@
+import domain.Tag;
+import domain.TaskDescription;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TasksTest {
-    // Alle Methoden der Taskklasse sind mit maximaler Code-Coverage zu testen
+    // Alle Methoden der Taskklasse sind mit maximaler Code-Coverage zu testen^
     // Es sind an passenden Stellen parametrisierte Tests zu verwenden
+
+    @Test
+    public void TagListHolen() throws IllegalArgumentException {
+
+        List<Tag> list = new ArrayList<>();
+        list.add(new Tag("Tag 1"));
+        Assertions.assertEquals(list, (list));
+
+    }
+
+    @Test
+    public void DescriptionHolen() throws IllegalArgumentException {
+
+        List<TaskDescription> descr = new ArrayList<>();
+        descr.add(new TaskDescription("Dies ist eine Beschreibung vom Task 1"));
+        Assertions.assertEquals(descr, (descr));
+
+    }
+
 }
