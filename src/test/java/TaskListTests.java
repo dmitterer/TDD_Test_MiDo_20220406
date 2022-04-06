6ee1ea10cfcb9b9e91749182898ed8a7f230eefd
Dominik <dmitterer@tsn.at>
2022-04-06 10:33:17 +0200

    Implementierung von "TasksTest"

--- src/test/java/TaskListTests.java
+++ src/test/java/TaskListTests.java
@@ -1,5 +1,22 @@
+import domain.TaskList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskListTests {
     // Alle Methoden der Taskliste sind mit maximaler Code-Coverage zu testen
     // Es sind an passenden Stellen parametrisierte Tests zu verwenden
     // Die importData-Methode muss mit Mock getestet werden.
+
+
+    @Test
+    public void ZeigeTaskList() throws IllegalArgumentException {
+
+        List<TaskList> list = new ArrayList<>();
+        list.add(new TaskList());
+        Assertions.assertEquals(list, (list));
+    }
+
 }
